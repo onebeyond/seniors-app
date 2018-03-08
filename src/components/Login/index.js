ee6fdd2090afb3910d5a0460c558edf7bd86c2c6
Ismael Bakkali Chairi <ismaelbakkalichairi@Ismaels-MBP-2.home>
2018-03-08 15:09:39 +0100

    Basic setup navigation

--- src/components/Login/index.js
+++ src/components/Login/index.js
@@ -1,14 +1,14 @@
-import Login from './template';
-import { connect } from 'react-redux';
+import Login from "./template";
+import { connect } from "react-redux";
 
-import {sendUserData} from '../../actions/form'
+import { sendUserData } from "../../actions/form";
 
 const mapDispatchToProps = dispatch => ({
-    sendUserData: (userData) => dispatch(sendUserData(userData))
-  });
-  
-  const mapStateToProps = state => ({
-    isLoaded: state.isLoaded
-  });
-  
+  sendUserData: userData => dispatch(sendUserData(userData))
+});
+
+const mapStateToProps = state => ({
+  isLoaded: state.isLoaded
+});
+
 export default connect(mapStateToProps, mapDispatchToProps)(Login);
